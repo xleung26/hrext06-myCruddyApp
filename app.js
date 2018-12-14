c5e6fcff5fc033011cd4d8457e320aea81200146
JohnMichelin <jmichelinphone@gmail.com>
2018-12-14 15:27:14 -0800

    feat(jquery): read text-entry value

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
   console.log('jQuery loaded');
+
   // write to local storage ????
-    //
+    // read the input value
+    $('.text-entry').val();
+    // store the input value locally
+  // how do we validate local storage ???
+  // when button is clicked, read the value above
 });
