bc9d59f91b56cf7d5ad6c5bbd817892f78170360
JohnMichelin <jmichelinphone@gmail.com>
2018-12-14 15:37:54 -0800

    feat(localStorage): write value of text-entry to localStorage

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ $(document).ready(function(){
 
   // write to local storage ????
     // read the input value
-    $('.text-entry').val();
+    $('.text-entry').val('this is a value');
+    localStorage.setItem('test of text-entry value', $('.text-entry').val());
+    // works like an object, will not remove
+    // previously added data by calling setItem on a
+    // new keyName
     // store the input value locally
   // how do we validate local storage ???
   // when button is clicked, read the value above
