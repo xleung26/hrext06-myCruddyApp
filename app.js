a885fd366ad5daf3b30551b7f8d84cd54582738e
JohnMichelin <jmichelinphone@gmail.com>
2018-12-14 15:53:42 -0800

    feat(localStorage): read from input and write to local storage

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ $(document).ready(function(){
   console.log('jQuery loaded');
 
   // write to local storage ????
-    // read the input value
-    $('.text-entry').val('this is a value');
-    localStorage.setItem('test of text-entry value', $('.text-entry').val());
-    // works like an object, will not remove
-    // previously added data by calling setItem on a
-    // new keyName
-    // store the input value locally
-  // how do we validate local storage ???
+    /*
+    NOTE
+    works like an object, will not remove
+     previously added data by calling setItem on a
+    new keyName
+  */
+    //store the input value locally
+    // will it overwrite when new text is entered - yes it will (sweet)
+
+
   // when button is clicked, read the value above
+  $('.btn-submit').on('click', function(){
+    localStorage.setItem('test of text-entry value', $('.text-entry').val());
+  });
+
 });
