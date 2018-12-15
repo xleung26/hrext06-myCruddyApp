8ddae0f1bad579f1fc2ec1bc906960ec15329be7
JohnMichelin <jmichelinphone@gmail.com>
2018-12-14 16:11:44 -0800

    feat(localStorage): add delete function along with button

--- README.md
+++ README.md
@@ -9,9 +9,19 @@ Create Read Update Delete using localStorage with JS, HTML and CSS
 - [x] Form with input fields
 - [x] Write to local storage
 - [x] Read from local storage
-- [ ] Edit local storage
-- [ ] Delete local storage
+- [x] Edit local storage
+    - [ ] What about if we have more than one value?
+    - [ ] how do we add multiple values?
+        - [ ] maybe use an array?
+        - [ ] maybe use multiple keys? create new keyname each time
+
+- [x] Delete local storage
+    - [x] delete button storage.removeItem()
+    - [ ] delete all storage.clear()
+- [ ] Display stored value in proper div
 
 ### Next Sprint
+- [ ] can we test this?
 - [ ] Styling
 - [ ] UX improvements
+    - [ ] capture/prevent invalid data
--- app.js
+++ app.js
@@ -1,25 +1,17 @@
 $(document).ready(function(){
   console.log('jQuery loaded');
 
-  // write to local storage ????
-    /*
-    NOTE
-    works like an object, will not remove
-     previously added data by calling setItem on a
-    new keyName
-  */
-    //store the input value locally
-    // will it overwrite when new text is entered - yes it will (sweet)
-
-
-  // when button is clicked, read the value above
+  // write to local storage from input when button save clicked
   $('.btn-submit').on('click', function(){
-    localStorage.setItem('test of text-entry value', $('.text-entry').val());
+    localStorage.setItem('inputFieldValue', $('.text-entry').val());
     var myItemInStorage = localStorage.getItem('test of text-entry value');
     console.log('myItemInStorage', myItemInStorage);
-
   });
-  
+
+  // delete from local storage when delete button clicked
+  $('.btn-delete').on('click', function(){
+    localStorage.removeItem('inputFieldValue');
+  });
   
 
 });
